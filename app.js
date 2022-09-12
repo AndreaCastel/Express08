b05b1dec90dd1b50ea6a4f3c6db446d3de25dd2b
AndreaCastel <marion.chateau@sncf.fr>
2022-09-12 14:30:14 +0200

    balek

---
+++ app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
+const isItDwight = (req, res) => {
+  if (req.body.email === 'dwight@theoffice.com' && req.body.password === "123456") {
+    res.send('credentials are valid');
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.post("/api/login", isItDwight);
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
